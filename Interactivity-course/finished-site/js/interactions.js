923b86b419d5743536a96b5914e70315a9ab303e
sam-osb <samuel.osborne@inheart.fr>
2021-04-11 19:01:32 +0200

    updated finished site

--- Interactivity-course/finished-site/js/interactions.js
+++ Interactivity-course/finished-site/js/interactions.js
@@ -1,16 +1,18 @@
 LottieInteractivity.create({
 	mode:"scroll",
-	player:'#lighthouse',
-	actions: [{
-		visibility:[0,1],
-		type: "seek",
-		frames: [0, 300],
-	},]
+	player:'#lottie-lighthouse',
+	actions: [
+		{
+			visibility:[0,1],
+			type: "seek",
+			frames: [0, 300],
+		},
+	]
 });
 
 LottieInteractivity.create({
 	mode:"cursor",
-	player: "#blueprint",
+	player: "#blueprint-lottie",
 	actions: [
 		{
 			position: { x: [0, 1], y: [0, 1] },
